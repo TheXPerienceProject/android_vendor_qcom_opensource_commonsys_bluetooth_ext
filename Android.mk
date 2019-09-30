b77f5b64f9c22a2dd1eb984dd2525f3889b992b4
klozz <klozz@vivaldi.net>
2019-09-30 05:23:39 +0000

    dont call bt test app
    Change-Id: Ie2759f60dbe22a8f0157f7eb9cfb922fa10d0569
    Signed-off-by: klozz <klozz@vivaldi.net>

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 TMP_LOCAL_PATH := $(LOCAL_PATH)
 
-include $(LOCAL_PATH)/BATestApp/Android.mk
+#include $(LOCAL_PATH)/BATestApp/Android.mk
 include $(TMP_LOCAL_PATH)/certification_tools/Android.mk
 include $(TMP_LOCAL_PATH)/system_bt_ext/btconfigstore/Android.mk
 
